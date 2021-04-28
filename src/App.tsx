7b7d1ef5998e2889ef09c59a31fae42903de89aa
Ariel <arielfebrian98@gmail.com>
2021-04-28 16:35:06 +0700

    penambahan tombol add item dan cart item agar bisa ditambahkan

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {useQuery} from 'react-query'
 
 //components
 import Item from './Item/item'
+import Cart from './Cart/Cart'
 import Drawer from '@material-ui/core/Drawer'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import Grid from '@material-ui/core/Grid'
@@ -11,7 +12,7 @@ import AddShoppingChartIcon from '@material-ui/icons/AddShoppingCart'
 import Badge from '@material-ui/core/Badge'
 
 //style
-import { Wrapper } from './App.styles'
+import { Wrapper, StyledButton } from './App.styles'
 
 //types
 export type CartItemType = {
@@ -31,11 +32,28 @@ const getProducts = async ():Promise<CartItemType[]> =>
 
 const App = () =>  {
 
+  const [cartOpen, setCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
   const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts )
-  console.log(data)
 
-  const getTotalItem = () => null 
-  const handleAddToCart = (clickedItem: CartItemType) => null 
+  const getTotalItem = (items: CartItemType[]) => 
+    items.reduce((ack: number, item) => ack + item.amount, 0) 
+
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      const itemIncart = prev.find(item => item.id === clickedItem.id)
+      
+      if(itemIncart){
+        return prev.map(item => 
+          item.id === clickedItem.id ? {...item, amount: item.amount + 1} : item )
+      }
+
+      //first time the item is added
+      return [...prev,  {...clickedItem, amount: 1 }]
+
+    })
+  } 
+
   const handleRemoveFromCart = () => null
   
   if(isLoading) return <LinearProgress/>
@@ -43,6 +61,19 @@ const App = () =>  {
 
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose = {()=>setCartOpen(false)} >
+        <Cart  
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart} />
+      </Drawer>
+
+      <StyledButton onClick = {() => setCartOpen(true)} >
+        <Badge badgeContent={ getTotalItem(cartItems) } color='error' >
+          <AddShoppingChartIcon/>
+        </Badge>
+      </StyledButton>
+
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4} >
