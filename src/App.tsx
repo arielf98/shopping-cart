d745a3e92a76953c952025519ecd3913bf87350b
Ariel <arielfebrian98@gmail.com>
2021-04-28 14:42:46 +0700

    penarikan data dari api

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState} from 'react'
+import {useQuery} from 'react-query'
+
+//components
+import Item from './Item/item'
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid'
+import AddShoppingChartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
+
+//style
+import { Wrapper } from './App.styles'
+
+//types
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number
+}
+
+//Api 
+const getProducts = async ():Promise<CartItemType[]> => 
+  await (await fetch('https://fakestoreapi.com/products')).json()
+
+
+const App = () =>  {
+
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts )
+  console.log(data)
+
+  const getTotalItem = () => null 
+  const handleAddToCart = (clickedItem: CartItemType) => null 
+  const handleRemoveFromCart = () => null
+  
+  if(isLoading) return <LinearProgress/>
+  if(error) return <div> Something Wrong.... </div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4} >
+              <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
